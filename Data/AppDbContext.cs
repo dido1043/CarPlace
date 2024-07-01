f2fc174097a24053f5888397fd1d7c349c1ae733
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-02 00:43:36 +0300

    Added Context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace CarPlace.Data
+{
+    public class AppDbContext:IdentityDbContext<IdentityUser>
+    {
+
+    }
+}
