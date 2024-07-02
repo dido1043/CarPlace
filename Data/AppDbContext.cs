20aa6e1b5bda1610561ffb5d6b7384647a752192
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-02 17:55:18 +0300

    updated context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CarPlace.Models.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
@@ -7,6 +8,18 @@ namespace CarPlace.Data
 {
     public class AppDbContext:IdentityDbContext<IdentityUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {}
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Dealer> Dealers { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<ServiceRecord> ServiceRecords { get; set; }
+        public DbSet<TestDrive> TestDrives { get; set; }
 
     }
 }
