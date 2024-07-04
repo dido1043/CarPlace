61f7e5ec95518cc7f77c2011b24943b3162b63ae
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-04 18:36:18 +0300

    up

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CarPlace.Models.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace CarPlace.Data.Models
 {
     public class User:IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        
-        public string Password { get; set; }
+        public ICollection<Car> FavCars { get; set; }
 
     }
 }
