b829a70e451c82e753d2e882e45b0fa5b0e6eef0
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-13 18:40:13 +0300

    account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,44 +1,47 @@
-﻿//using CarPlace.Data;
-//using CarPlace.Data.Models;
-//using CarPlace.Data.Models.IdentityModels;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
+﻿using CarPlace.Data;
+using CarPlace.Data.Models;
+using CarPlace.Data.Models.RequestModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace CarPlace.Controllers
-//{
-//    public class AccountController : Controller
-//    {
-//        private readonly UserManager<User> _userManager;
-//        private readonly AppDbContext _context;
+namespace CarPlace.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly AppDbContext _context;
 
-//        public AccountController(UserManager<User> userManager, AppDbContext context)
-//        {
-//            this._userManager = userManager;
-//            this._context = context;
-//        }
-//        [HttpPost]
-//        [Route("register")]
-//        public async Task<IActionResult> Register(RegisterModel request)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest(ModelState);
-//            }
+        public AccountController(UserManager<User> userManager, AppDbContext context)
+        {
+            this._userManager = userManager;
+            this._context = context;
+        }
 
-//            var result = await _userManager.CreateAsync(
-//                new User 
-//                { 
-//                    FirstName = request.FirstName,
-//                    LastName = request.LastName,
-//                    Email = request.Email
-//                },
-//                request.Password);
-//            if (!result.Succeeded)
-//            {
-//                return BadRequest(result);
-//            }
-//            return CreatedAtAction(nameof(Register), request);
-//        }
-//    }
-//}
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody]RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userManager.CreateAsync(
+                new User
+                {
+                    Email = request.Email,
+                },
+                request.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+            return CreatedAtAction(nameof(Register), request);
+        }
+        private static void JSONDeserializer()
+        {
+
+        }
+    }
+}
