764060499f5ac31e92c8157b5d8f241998891a8a
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-14 20:01:45 +0300

    Created DTOs

---
+++ Data/DTO/CarModels/CarDTO.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace CarPlace.Data.DTO.CarModels
+{
+    public class CarDTO
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("make")]
+        public string Make { get; set; }
+        [JsonProperty("model")]
+        public string Model { get; set; }
+        [JsonProperty("year")]
+        public int Year { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Data/DTO/RequestModels/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace CarPlace.Data.DTO.RequestModels
+{
+    public class RegisterRequest
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
