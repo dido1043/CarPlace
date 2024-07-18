2addced4bbbc6684bd54c20e1e511c7c1381dd51
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-18 19:03:56 +0300

    Created Add Action

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,5 +1,6 @@
 ﻿using CarPlace.Data;
 using CarPlace.Data.DTO.CarModels;
+using CarPlace.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,16 +19,41 @@ namespace CarPlace.Controllers
         public async Task<IActionResult> All()
         {
             var cars = await _context.Cars
-                .Select(c => new CarDTO 
+                .Select(c => new CarDTO
                 {
                     Id = c.Id,
                     Make = c.Make,
                     Model = c.Model,
                     Year = c.Year,
                     Price = c.Price,
+                    ImageUrl = c.ImageUrl,
                 }).ToListAsync();
 
             return CreatedAtAction(nameof(All), cars);
+        }
+        [HttpPost]
+        [Route("/cars/add")]
+        public async Task<IActionResult> Add(CarDTO car)
+        {
+
+            var entity = new Car()
+            {
+                Id = car.Id,
+                Make = car.Make,
+                Model = car.Model,
+                Year = car.Year,
+                Price = car.Price,
+                ImageUrl = car.ImageUrl,
+                
+
+            };
+            if (!ModelState.IsValid)
+            {
+                throw new Exception("Invaid car data!");
+            }
+            _context.Cars.Add(entity);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Add), entity);
         }
     }
 }
