b53ccd39efd277e675a941c244542e05e4aa44be
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-03 18:24:10 +0300

    Added migration for cars model with 2 new properties(HP and Description)

---
+++ Data/Migrations/20240803104706_addedHPAndDescription.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarPlace.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedHPAndDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "HP",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "HP",
+                table: "Cars");
+        }
+    }
+}
