f82404d6cdebffb2ef86b191282ecb2d84c54958
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-11 21:33:27 +0300

    up

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,41 +3,46 @@ using CarPlace.Data.DTO.RequestModels;
 using CarPlace.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Runtime.InteropServices;
 namespace CarPlace.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly AppDbContext _context;
 
-        public AccountController(UserManager<User> userManager, AppDbContext context)
+        public AccountController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, AppDbContext context)
         {
             this._userManager = userManager;
+            this._roleManager = roleManager;
             this._context = context;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody]RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
-            var result = await _userManager.CreateAsync(
-                new User
-                {
-                    Email = request.Email,
-                },
-                request.Password);
-
-            if (!result.Succeeded)
+            var appUser = new User
             {
-                return BadRequest(result);
-            }
+                Email = request.Email,
+            };
+            
+            var result = await _userManager.CreateAsync(appUser, request.Password);
+
+
+
+
             return CreatedAtAction(nameof(Register), request);
         }
-       
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+
+            return Ok();
+        }
     }
 }
