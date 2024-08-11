727822c054371537455963048fdef6d7f98a912a
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-11 21:33:35 +0300

    Added roles

---
+++ Managers/CustomUserManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace CarPlace.Managers
+{
+    public class CustomUserManager<TUser> : UserManager<TUser> where TUser : class
+    {
+        public CustomUserManager(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<TUser> passwordHasher,
+            IEnumerable<IUserValidator<TUser>> userValidators, IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<TUser>> logger)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+        }
+
+        public override async Task<IdentityResult> CreateAsync(TUser user, string password)
+        {
+            var result = await base.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                var defaultRole = "Client"; // Change to your default role name
+                await AddToRoleAsync(user, defaultRole);
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 //using CarPlace.Configuration;
 using CarPlace.Data;
 using CarPlace.Data.Models;
+using CarPlace.Managers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CarPlace
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public  static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -17,20 +20,43 @@ namespace CarPlace
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
-            builder.Services.AddIdentityCore<User>()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddApiEndpoints();
-
-            builder.Services.Configure<IdentityOptions>(options =>
+            builder.Services.AddScoped<UserManager<User>, CustomUserManager<User>>();
+            builder.Services.AddIdentityApiEndpoints<User>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
                 options.Password.RequireDigit = false;
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequireUppercase = false;
-            });
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints();
+            //builder.Services.AddScoped<UserManager<User>, CustomUserManager<User>>();
+            //builder.Services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
 
+            //}).AddJwtBearer(opt =>
+            //{
+            //    opt.TokenValidationParameters = new TokenValidationParameters()
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidIssuer = builder.Configuration["JWT:Issuer"],
+            //        ValidateAudience = true,
+            //        ValidAudience = builder.Configuration["JWT:Audience"],
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(
+            //         System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+            //        )
+            //    };
+            //});
 
-            builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+            builder.Services.AddAuthentication();//.AddBearerToken(IdentityConstants.BearerScheme);
             builder.Services.AddAuthorizationBuilder();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -75,9 +101,20 @@ namespace CarPlace
             {
                 endpoints.MapControllers();
             });
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var roles = new[] { "Admin", "Client" };
 
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+            }
             //app.MapControllers();
-
             app.Run();
         }
     }
