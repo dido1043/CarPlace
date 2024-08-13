dd59dacb2d44450f8c2f6a8ca8c4cb2f6eb9a603
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-13 20:34:10 +0300

    Added 2fa ignore filter

---
+++ Filters/IgnorePropertiesFilter .cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CarPlace.Filters
+{
+    public class IgnorePropertiesFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            // Remove properties from the request example that are not needed
+            var propertiesToIgnore = new List<string> { "twoFactorCode", "twoFactorRecoveryCode" };
+
+            if (operation.RequestBody?.Content.Values is IList<OpenApiMediaType> contentMediaTypes)
+            {
+                foreach (var mediaType in contentMediaTypes)
+                {
+                    if (mediaType.Schema?.Properties != null)
+                    {
+                        foreach (var property in propertiesToIgnore)
+                        {
+                            mediaType.Schema.Properties.Remove(property);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
