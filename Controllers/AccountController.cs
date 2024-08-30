8b0e5311260495f4fff7aa8ae7fa0f076ae4bd08
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-30 20:16:40 +0300

    up

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,20 @@
 ﻿using CarPlace.Data;
 using CarPlace.Data.DTO.RequestModels;
+using CarPlace.Data.DTO.ResponseModels;
 using CarPlace.Data.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol.Plugins;
+using System.Runtime.CompilerServices;
 namespace CarPlace.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly SignInManager<User> _signInManager;    
+        private readonly SignInManager<User> _signInManager;
         private readonly AppDbContext _context;
 
         public AccountController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
@@ -33,25 +38,54 @@ namespace CarPlace.Controllers
             {
                 Email = request.Email,
             };
-            
+
             var result = await _userManager.CreateAsync(appUser, request.Password);
 
             return CreatedAtAction(nameof(Register), request);
         }
+
+
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        public async Task<ActionResult<LoginResponseModel>> Login([FromBody] LoginRequest request)
         {
             var user = await _userManager.FindByEmailAsync(request.Email);
             if (user == null)
             {
                 throw new Exception("Invalid user!");
             }
-            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password,false, false);
+
+            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, false);
             if (!result.Succeeded)
             {
-                throw new Exception("Invalid result");
+                throw new Exception("Invalid credentials");
             }
-            return Ok();
+
+            var role = await GetUserRole(user);
+
+            
+            var tokenType = "Bearer";
+            var accessToken = await _userManager.GenerateUserTokenAsync(user, "CarPlace", role);
+            var expiresIn = 3600; 
+            var refreshToken = await _userManager.GenerateUserTokenAsync(user, "CarPlace", role);
+
+            var response = new LoginResponseModel
+            {
+                TokenType = tokenType,
+                AccessToken = accessToken,
+                ExpiresIn = expiresIn,
+                RefreshToken = refreshToken,
+                Role = role,
+            };
+            return response;
         }
+
+        private async Task<string> GetUserRole(User user)
+        {
+
+            var rolename = await _userManager.GetRolesAsync(user);
+            return rolename.FirstOrDefault();
+        }
+
     }
 }
+
---
+++ Data/DTO/ResponseModels/LoginResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace CarPlace.Data.DTO.ResponseModels
+{
+    public class LoginResponseModel
+    {
+        [JsonProperty("tokenType")]
+        public string TokenType { get; set; }
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+        [JsonProperty("expiresIn")]
+        public int ExpiresIn { get; set; }
+        [JsonProperty("refreshToken")]
+        public string RefreshToken { get; set; }
+        [JsonProperty("role")]
+        public string Role { get; set; }
+    }
+}
