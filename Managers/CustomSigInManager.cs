feefa8a05fcf86e3319ec40ade610330d6dd4e0a
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-03 19:09:14 +0300

    up

---
+++ Managers/CustomSigInManager.cs
@@ -0,0 +1,23 @@
+﻿using CarPlace.Data.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+public class CustomSignInManager<TUser> : SignInManager<User>
+{
+    public CustomSignInManager(UserManager<User> userManager, IHttpContextAccessor contextAccessor,
+        IUserClaimsPrincipalFactory<User> claimsFactory, 
+        IOptions<IdentityOptions> optionsAccessor, 
+        ILogger<SignInManager<User>> logger, 
+        IAuthenticationSchemeProvider schemes, 
+        IUserConfirmation<User> confirmation)
+        : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+    {
+    }
+
+    public override async Task SignInAsync(User user, AuthenticationProperties authenticationProperties, string authenticationMethod = null)
+    {
+        // Custom sign-in logic if needed
+        await base.SignInAsync(user, authenticationProperties, authenticationMethod);
+    }
+}
