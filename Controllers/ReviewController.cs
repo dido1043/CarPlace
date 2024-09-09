35244097172e6993cc7e41d20c0e2dc4eb460e42
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-09 18:08:16 +0300

    Created controller for car reviews

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace CarPlace.Controllers
+{
+    [Route("[controller]")]
+    public class ReviewController : Controller
+    {
+        [HttpGet]
+        [Route("cars/reviews/all")]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("cars/reviews/add")]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+        
+    }
+}
