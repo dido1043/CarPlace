dc6de3660432bfb471bad17cb3fc0cf58e339ec5
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-13 15:14:58 +0300

    Ready "All" and "Add" methods for the ReviewController

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarPlace.Data;
+using CarPlace.Data.DTO.ReviewModels;
+using CarPlace.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Security.Policy;
 
 namespace CarPlace.Controllers
@@ -6,18 +11,53 @@ namespace CarPlace.Controllers
     [Route("[controller]")]
     public class ReviewController : Controller
     {
+        private readonly AppDbContext _context;
+        public ReviewController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         [Route("cars/reviews/all")]
         public async Task<IActionResult> All()
         {
-            return View();
+            var currentUser = User.Identity?.Name;
+            var reviews = await _context.Reviews.Select(c => new ReviewDTO
+            {
+
+                CarId = c.CarId,
+                Customer = currentUser,
+                Content = c.Content,
+                Rating = c.Rating,
+
+            }).ToListAsync();
+            return Ok(reviews);
         }
         [HttpPost]
         [Route("cars/reviews/add")]
-        public async Task<IActionResult> Add()
+        public async Task<IActionResult> Add([FromBody] ReviewDTO review)
         {
-            return View();
+            var currentCar = await _context.Cars.FindAsync(review.CarId);
+
+            var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            if (currentCar == null)
+            {
+                return NotFound("Car not found");
+            }
+
+            var reviewEntity = new Review
+            {
+                CarId = currentCar.Id,
+                CustomerId = currentUserId,
+                Content = review.Content,
+                Rating = review.Rating,
+            };
+
+            _context.Reviews.Add(reviewEntity);
+            await _context.SaveChangesAsync();
+            return Ok(reviewEntity);
         }
-        
+
     }
 }
