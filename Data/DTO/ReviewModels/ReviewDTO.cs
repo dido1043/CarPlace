9be93cf0c60e2c5b62e4da9ffeba7f79b525e936
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-13 15:13:56 +0300

    Created ReviewDto

---
+++ Data/DTO/ReviewModels/ReviewDTO.cs
@@ -0,0 +1,22 @@
+﻿using CarPlace.Data.Models;
+using CarPlace.Models.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace CarPlace.Data.DTO.ReviewModels
+{
+    public class ReviewDTO
+    {
+        [JsonProperty("carId")]
+        public int CarId { get; set; }
+        [JsonProperty("customer")]
+        public string Customer { get; set; }
+        [JsonProperty("content")]
+        public string Content { get; set; }
+        [JsonProperty("property")]
+        public int Rating { get; set; }
+    }
+}
