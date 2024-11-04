b96a2c1235badc38c42491db1d255a4f6d8e4dbf
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-11-04 16:56:34 +0200

    Added Rent request

---
+++ Data/Models/RentRequest.cs
@@ -0,0 +1,19 @@
+﻿using CarPlace.Models.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarPlace.Data.Models
+{
+    public class RentRequest
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        public int CarId { get; set; }
+        [ForeignKey(nameof(CarId))]
+        public Car Car { get; set; }
+    }
+}
