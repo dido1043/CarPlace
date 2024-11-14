3041010dfe8d42eb77ee773da422f5b1c1e10c8b
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-11-14 23:13:49 +0200

    up

---
+++ Data/DTO/RentRequestModel/RentRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CarPlace.Data.DTO.RentRequestModel
+{
+    
+    public class RentRequestModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("user")]
+        public string User { get; set; }
+        [JsonProperty("carId")]
+        public int CarId { get; set; }
+    }
+}
