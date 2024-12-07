dcfcacc4aa65c98f53c50c1b338046e4b3779f84
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-12-07 18:31:45 +0200

    Started CarFeature

---
+++ Controllers/CarFeatureController.cs
@@ -0,0 +1,27 @@
+﻿using CarPlace.Data.DTO.CarFeatureModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPlace.Controllers
+{
+    public class CarFeatureController : Controller
+    {
+        [HttpGet]
+        [Route("/cars/features/all")]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/cars/features/add")]
+        public async Task<IActionResult> Add([FromBody] CarFeatureDTO carFeature)
+        {
+            return Ok(carFeature);
+        }
+        [HttpPut]
+        [Route("/cars/features/edit/{cfId}")]
+        public async Task<IActionResult> Edit(int cfId, [FromBody] CarFeatureDTO carFeature) 
+        { 
+            return Ok(carFeature);
+        }
+    }
+}
---
+++ Data/DTO/CarFeatureModel/CarFeatureDTO.cs
@@ -0,0 +1,18 @@
+﻿using CarPlace.Models.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace CarPlace.Data.DTO.CarFeatureModel
+{
+    public class CarFeatureDTO
+    {
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("carId")]
+        public int CarId { get; set; }
+        [JsonProperty("feature")]
+        public string Feature { get; set; }
+    }
+}
