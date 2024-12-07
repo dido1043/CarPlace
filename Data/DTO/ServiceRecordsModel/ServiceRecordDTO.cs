256ddec3bd46c7067bbabcb1217213bab12a2de1
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-12-07 18:30:04 +0200

    ready service record DTO

---
+++ Data/DTO/ServiceRecordsModel/ServiceRecordDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CarPlace.Data.DTO.ServiceRecordsModel
+{
+    public class ServiceRecordDTO
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("carId")]
+        public int CarId { get; set; }
+        [JsonProperty("serviceDate")]
+        public DateTime ServiceDate { get; set; }
+        [JsonProperty("serviceDetails")]
+        public string ServiceDetails { get; set; }
+    }
+}
